03bb8fa4b5da213e5900bd2b657d8093bf1626de
nicoleleft <wrightnicole2@yahoo.com>
2023-03-04 18:51:35 -0500

    first commit

---
+++ custom_modules/plant_care_app/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Plant Care App',
+    'version': '1.0',
+    'category': 'Productivity',
+    'summary': 'Plant watering schedule tracker',
+    'sequence': -50,
+    'description': """This application is a watering schedule and fertilizer schedule tracking module used to notify users of when they should be watering and feeding their plants""",
+    'author': 'Nicole W',
+    'installable': True,
+    'application': True,
+}
