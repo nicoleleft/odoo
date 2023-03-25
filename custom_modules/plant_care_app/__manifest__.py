f45c9141ec6870bb532a7d3019037f4dbb82b89d
nicoleleft <wrightnicole2@yahoo.com>
2023-03-25 16:48:13 -0400

    fix views, add barebones to security files

--- custom_modules/plant_care_app/__manifest__.py
+++ custom_modules/plant_care_app/__manifest__.py
@@ -8,4 +8,8 @@
     'author': 'Nicole W',
     'installable': True,
     'application': True,
+    'data': [
+                "views/plant.xml",
+                "security/plant_security.xml"
+            ]
 }
--- custom_modules/plant_care_app/models/plant.py
+++ custom_modules/plant_care_app/models/plant.py
@@ -1,7 +1,8 @@
-from server.odoo import models, fields
+from odoo import models, fields
 
 class Plants(models.Model):
     _name = 'plant'
 
     name = fields.Char(string = "Plant name")
     watering_schedule = fields.Integer()
+    time_unit = fields.Selection([('days', 'Days'), ('weeks', 'Weeks'), ('months', 'Months')])
