e6ee5247be1679e5e36513767350b0606da6a1d1
nicoleleft <wrightnicole2@yahoo.com>
2023-04-28 09:03:51 -0400

    add one2many field in users tab

--- custom_modules/plant_care_app/models/users.py
+++ custom_modules/plant_care_app/models/users.py
@@ -5,18 +5,7 @@ class Users(models.Model):
     _name = 'users'
     _description = 'Users record'
 
-    name = fields.Char(string = "Name")
+    name = fields.Many2one("res.users", string = "Name")
     id = fields.Integer(string = "User ID")
     plants = fields.Char(string="Plants")
 
-    #   Helper function to display list of plants?
-    @api.model
-    def create_demo_data(self):
-        with open('/data/plant_parents.csv') as csvfile:
-            reader = csv.DictReader(csvfile)
-            for row in reader:
-                self.env['users'].create({
-                    'name': row['name'],
-                    'id': row['id'],
-                    'plants': row['plants']
-                })
